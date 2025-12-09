b259399640f372a37aac770a37a16245ec669c40
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-09 15:10:19 +0530

    csscds

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-import expess from 'express';
-import dotenv from 'dotenv';
-import ConnectDB from './config/Db.js';
-import userRouter from './User/UserRouter.js';
-import cors from 'cors';
-import TemplateRoute from './Tamplete/TampleteRoute.js';
-import MessageApiRoute from './Message/MessageRoute.js';
+import expess from "express";
+import dotenv from "dotenv";
+import ConnectDB from "./config/Db.js";
+import userRouter from "./User/UserRouter.js";
+import cors from "cors";
+import TemplateRoute from "./Tamplete/TampleteRoute.js";
+import MessageApiRoute from "./Message/MessageRoute.js";
 dotenv.config();
 
 const app = expess();
-ConnectDB()
+ConnectDB();
 app.use(cors());
 
-app.use('/', (req, res) => {
-  res.send('👍  API is running....');
-});
 
 
 // JSON parsing for routes (excluding file upload)
-app.use('/api/user', (req, res, next) => {
-  if (req.path === '/uploadFile') {
+app.use("/api/user", (req, res, next) => {
+  if (req.path === "/uploadFile") {
     return next();
   }
-  expess.json({ limit: '50mb' })(req, res, next);
+  expess.json({ limit: "50mb" })(req, res, next);
 });
 
-app.use(expess.urlencoded({ extended: true, limit: '50mb' }));
-app.use(expess.json({ limit: '50mb' }));
+app.use(expess.urlencoded({ extended: true, limit: "50mb" }));
+app.use(expess.json({ limit: "50mb" }));
 
-app.use('/api/user', userRouter);
-app.use('/api/templates', TemplateRoute);
-app.use('/api/message-reports', MessageApiRoute);
-const PORT = process.env.PORT || 3000; 
+app.use("/api/user", userRouter);
+app.use("/api/templates", TemplateRoute);
+app.use("/api/message-reports", MessageApiRoute);
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
